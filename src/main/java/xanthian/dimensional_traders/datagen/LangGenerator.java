d9e0dff167c935995ea79db87ceb3d72b3e4094c
Xanthian <xanneh@hotmail.com>
2023-12-22 15:03:47 +0000

    1.20.1 1.1.1 fix lang issue

--- src/main/java/xanthian/dimensional_traders/datagen/LangGenerator.java
+++ src/main/java/xanthian/dimensional_traders/datagen/LangGenerator.java
@@ -3,6 +3,7 @@ package xanthian.dimensional_traders.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import xanthian.dimensional_traders.block.ModBlocks;
+import xanthian.dimensional_traders.entity.ModVillagers;
 
 public class LangGenerator extends FabricLanguageProvider {
     public LangGenerator(FabricDataOutput dataOutput) {
@@ -14,5 +15,9 @@ public class LangGenerator extends FabricLanguageProvider {
 
         translationBuilder.add(ModBlocks.END_TRADER_STALL, "End Trading Stall");
         translationBuilder.add(ModBlocks.NETHER_TRADER_STALL, "Nether Trading Stall");
+
+        translationBuilder.add(ModVillagers.END_TRADER_ID, "End Trader");
+        translationBuilder.add(ModVillagers.NETHER_TRADER_ID, "Nether Trader");
+
     }
 }
